829c295dd035ee3afa6d7f955c01f8d38d47d90a
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-09-25 10:28:09 -0700

    Add install-deps.sh script

---
+++ install-deps.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+sudo apt-get update
+sudo apt-get install -y python-pip \ 
+                        python3-pip \ 
+                        python3-venv
+ 
+pip3 install Cython
+
+sudo apt-get install -y python3-sphinx \ 
+                        libibverbs-dev \ 
+                        libudev-dev \ 
+                        libblkid-dev \ 
+                        libkeyutils-dev \ 
+                        libldap-dev \ 
+                        libfuse-dev \ 
+                        libcryptsetup-dev \ 
+                        libaio-dev \ 
+                        libsqlite3-dev \ 
+                        libsnappy-dev \ 
+                        liblz4-dev \ 
+                        libgoogle-perftools-dev \ 
+                        libcurl4-openssl-dev \ 
+                        liboath-dev \ 
+                        liblttng-ust-dev \ 
+                        libbabeltrace-dev \ 
+                        libthrift-dev \ 
+                        liblua5.3-dev \
+                        libnl-genl-3-dev \ 
+                        libcap-ng-dev \ 
+                        libicu-dev \ 
+                        librabbitmq-dev \ 
+                        librdkafka-dev
+
