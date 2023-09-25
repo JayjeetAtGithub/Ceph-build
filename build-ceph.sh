330b398ed3ad2139631d00406954a1acee03c241
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-09-25 10:41:24 -0700

    Add ceph build script

---
+++ build-ceph.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+if [ ! -d "ceph" ]; then
+    git clone https://github.com/ceph/ceph.git
+fi
+
+cd ceph
+git submodule update --init --recursive
+git checkout v18.2.0
+
+./do_cmake.sh
+cd build
+ninja
