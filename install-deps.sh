9e824fc882dd63966cb1820de7757c2662e0fa4e
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-09-25 10:35:17 -0700

    Fix script

--- install-deps.sh
+++ install-deps.sh
@@ -2,34 +2,34 @@
 set -ex
 
 sudo apt-get update
-sudo apt-get install -y python3-dev \ 
-                        python3-pip \ 
+sudo apt-get install -y python3-dev \
+                        python3-pip \
                         python3-venv \
                         python3-sphinx
  
 pip3 install Cython
 
-sudo apt-get install -y libibverbs-dev \ 
-                        libudev-dev \ 
-                        libblkid-dev \ 
-                        libkeyutils-dev \ 
-                        libldap-dev \ 
-                        libfuse-dev \ 
-                        libcryptsetup-dev \ 
-                        libaio-dev \ 
-                        libsqlite3-dev \ 
-                        libsnappy-dev \ 
-                        liblz4-dev \ 
-                        libgoogle-perftools-dev \ 
-                        libcurl4-openssl-dev \ 
-                        liboath-dev \ 
-                        liblttng-ust-dev \ 
-                        libbabeltrace-dev \ 
-                        libthrift-dev \ 
+sudo apt-get install -y libibverbs-dev \
+                        libudev-dev \
+                        libblkid-dev \
+                        libkeyutils-dev \
+                        libldap-dev \
+                        libfuse-dev \
+                        libcryptsetup-dev \
+                        libaio-dev \
+                        libsqlite3-dev \
+                        libsnappy-dev \
+                        liblz4-dev \
+                        libgoogle-perftools-dev \
+                        libcurl4-openssl-dev \
+                        liboath-dev \
+                        liblttng-ust-dev \
+                        libbabeltrace-dev \
+                        libthrift-dev \
                         liblua5.3-dev \
-                        libnl-genl-3-dev \ 
-                        libcap-ng-dev \ 
-                        libicu-dev \ 
-                        librabbitmq-dev \ 
+                        libnl-genl-3-dev \
+                        libcap-ng-dev \
+                        libicu-dev \
+                        librabbitmq-dev \
                         librdkafka-dev
 
