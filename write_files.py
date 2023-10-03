944059da33d838e690c6a6b0abf0d01d3697c5f6
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-10-03 14:15:22 -0700

    Add python script to write random files

---
+++ write_files.py
@@ -0,0 +1,7 @@
+import uuid
+
+if __name__ == "__main__":
+    for i in range(1000):
+        filename = uuid.uuid4()
+        with open(f"/mnt/cephfs/{filename}.txt", "w") as f:
+            f.write(filename)
