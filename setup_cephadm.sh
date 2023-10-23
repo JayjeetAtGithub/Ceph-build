bbbd22c420af1cc1bd8e77c8a8a6db505564d3e8
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-10-23 10:14:21 -0700

    install cephadm on all hosts

--- setup_cephadm.sh
+++ setup_cephadm.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 set -eu
 
-# Install dependencies
-curl --silent --remote-name --location https://github.com/ceph/ceph/raw/quincy/src/cephadm/cephadm
-chmod +x cephadm
-./cephadm add-repo --release quincy
-./cephadm install
-which cephadm
+HOSTS=${1:-node0}
+
+IFS=',' read -ra HOST_LIST <<< "$HOSTS"; unset IFS
+HOST_LIST=${HOST_LIST[@]}
+
+exec_cmd() {
+    echo "$1: $2"
+
+    hostname=$(hostname -s)
+    if [ "$hostname" == "$1" ]; then
+        bash $2
+        return
+    fi
+
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $1 $2
+}
+
+# install cephadm on hosts
+for node in ${HOST_LIST}; do
+    exec_cmd $node "curl --silent --remote-name --location https://github.com/ceph/ceph/raw/quincy/src/cephadm/cephadm"
+    exec_cmd $node "chmod +x cephadm"
+    exec_cmd $node "./cephadm add-repo --release quincy"
+    exec_cmd $node "./cephadm install"
+    exec_cmd $node "which cephadm"
+done
