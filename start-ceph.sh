2ffd750ce9560ab1bd00da18b0b86ea1707e3dce
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-10-26 12:48:19 -0700

    Move keyring to /etc/ceph

--- start-ceph.sh
+++ start-ceph.sh
@@ -4,4 +4,7 @@ set -ex
 cd ceph/build
 ninja vstart
 ../src/vstart --debug --new -x --localhost --bluestore --bluestore-devs /dev/nvme0n1
-./bin/ceph -s
+cp ceph.conf /etc/ceph
+cp keyring /etc/ceph
+ceph -s
+rados df
