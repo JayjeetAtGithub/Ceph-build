35b642e0cba0da998b8bca98d4addf8e9a51b3ca
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-10-26 16:09:05 -0700

    Add support for blkdev

--- start-ceph.sh
+++ start-ceph.sh
@@ -5,7 +5,7 @@ blkdev=$1
 
 cd ceph/build
 ninja vstart
-MON=1 OSD=1 MDS=1 MGR=1 ../src/vstart.sh --debug --new -x --localhost --bluestore --bluestore-devs /dev/${blkdev}
+MON=1 OSD=1 MDS=1 MGR=1 ../src/vstart.sh --debug --new -x --localhost --bluestore --bluestore-devs ${blkdev}
 cp ceph.conf /etc/ceph
 cp keyring /etc/ceph
 ./bin/ceph -s
