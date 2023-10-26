03655cf4236a5a3525a6b316279b903e114e0223
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-10-26 14:58:12 -0700

    Fix start-ceph script

--- start-ceph.sh
+++ start-ceph.sh
@@ -6,5 +6,5 @@ ninja vstart
 MON=1 OSD=1 MDS=1 MGR=1 ../src/vstart.sh --debug --new -x --localhost --bluestore --bluestore-devs /dev/nvme0n1
 cp ceph.conf /etc/ceph
 cp keyring /etc/ceph
-ceph -s
-rados df
+./bin/ceph -s
+./bin/rados df
