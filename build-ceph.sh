ad6452eb0f2bfdcafe908db939370cbe68bf299e
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-10-26 12:04:34 -0700

    Try to do a min build

--- build-ceph.sh
+++ build-ceph.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+# debug build
+
 if [ ! -d "ceph" ]; then
     git clone https://github.com/ceph/ceph.git
 fi
@@ -10,9 +12,11 @@ git submodule update --init --recursive
 git checkout v17.2.0
 
 ./install-deps.sh
+./run-make-check.sh
+
 sudo apt install -y cmake g++-13 libboost-all-dev
 
 rm -rf build
- ./do_cmake.sh
+./do_cmake.sh -DWITH_RBD=OFF -DWITH_KRBD=OFF -DWITH_RADOSGW=OFF -DWITH_MANPAGE=OFF -DWITH_BABELTRACE=OFF -DWITH_MGR_DASHBOARD_FRONTEND=OFF
 cd build
 ninja -j$(nproc)
