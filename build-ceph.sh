ecd301b77ddcf2ac5a011514d18367fdd0669c69
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-10-26 14:40:42 -0700

    Try to fix boost issue

--- build-ceph.sh
+++ build-ceph.sh
@@ -11,12 +11,13 @@ cd ceph
 git submodule update --init --recursive
 git checkout v17.2.0-oas
 
+sudo apt-get update
 ./install-deps.sh
 ./run-make-check.sh
 
 sudo apt install -y cmake libboost-all-dev ceph-common ceph-fuse
 
 rm -rf build
-./do_cmake.sh -DWITH_RDMA=OFF -DWITH_RBD=OFF -DWITH_KRBD=OFF -DWITH_RADOSGW=OFF -DWITH_MANPAGE=OFF -DWITH_BABELTRACE=OFF -DWITH_MGR_DASHBOARD_FRONTEND=OFF
+./do_cmake.sh -DWITH_SYSTEM_BOOST=ON -DWITH_RDMA=OFF -DWITH_RBD=OFF -DWITH_KRBD=OFF -DWITH_RADOSGW=OFF -DWITH_MANPAGE=OFF -DWITH_BABELTRACE=OFF -DWITH_MGR_DASHBOARD_FRONTEND=OFF
 cd build
 ninja -j$(nproc)
