fa034cbc325aade9f6d7566230611a773d0949fb
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-10-28 10:18:14 -0700

    Add conf and keyring to default paths

--- start-ceph.sh
+++ start-ceph.sh
@@ -6,5 +6,7 @@ blkdev=$1
 cd ceph/build
 ninja vstart
 MON=1 OSD=1 MDS=1 MGR=1 ../src/vstart.sh --debug --new -x --localhost --bluestore --bluestore-devs ${blkdev}
+cp ceph.conf /etc/ceph/ceph.conf
+cp keyring /etc/ceph/keyring
 ./bin/ceph -s
 ./bin/rados df
