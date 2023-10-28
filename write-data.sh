dbd4ecfadd7d4bcf0224de50ac06d17f39b57194
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-10-28 10:53:33 -0700

    don't download test file if already present

--- write-data.sh
+++ write-data.sh
@@ -4,7 +4,9 @@ set -ex
 mkdir -p /mnt/cephfs/dataset
 
 # download a test file
-wget http://link.testfile.org/150MB -O test.zip
+if [ ! -d "test.zip" ]; then
+    wget http://link.testfile.org/150MB -O test.zip
+fi
 
 # copy the file 100 times into /mnt/cephfs/dataset
 for i in {1..100}; do
