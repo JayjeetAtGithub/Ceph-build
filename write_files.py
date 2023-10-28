249b899c54d3e7b423e094e017b1ed363481a41c
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-10-28 11:01:41 -0700

    don't download test file if already present

--- write_files.py
+++ write_files.py
@@ -1,7 +1,7 @@
 import uuid
 
 if __name__ == "__main__":
-    for i in range(5000):
+    for i in range(100000):
         data = str(uuid.uuid4())
-        with open(f"/mnt/cephfs/{i}.txt", "w") as f:
+        with open(f"/mnt/cephfs/{data}.txt", "w") as f:
             f.write(data)
