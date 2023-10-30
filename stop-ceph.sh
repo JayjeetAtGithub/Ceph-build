97599be3851fe09e8b3f811de0de949532ee0e18
Jayjeet Chakraborty <jchakra1@ucsc.edu>
2023-10-30 14:01:30 -0700

    don't fail

--- stop-ceph.sh
+++ stop-ceph.sh
@@ -4,12 +4,12 @@ set -eu
 blkdev=$1
 
 # delete the cephfs
-fusermount -uz /mnt/cephfs
+fusermount -uz /mnt/cephfs || true
 rm -rf /mnt/cephfs
-ceph fs fail cephfs
-ceph fs rm cephfs --yes-i-really-mean-it
-ceph osd pool delete cephfs_data cephfs_data --yes-i-really-really-mean-it
-ceph osd pool delete cephfs_metadata cephfs_metadata --yes-i-really-really-mean-it
+ceph fs fail cephfs || true
+ceph fs rm cephfs --yes-i-really-mean-it || true
+ceph osd pool delete cephfs_data cephfs_data --yes-i-really-really-mean-it || true
+ceph osd pool delete cephfs_metadata cephfs_metadata --yes-i-really-really-mean-it || true
 
 cd ceph/build
 ../src/stop.sh
