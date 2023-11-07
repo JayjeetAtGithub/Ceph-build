60ab427c42e56cfd98b4cfb7e893832e0009089b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-11-07 14:15:48 -0800

    make /etc/ceph first

--- start-ceph.sh
+++ start-ceph.sh
@@ -6,6 +6,8 @@ blkdev=$1
 cd ceph/build
 ninja vstart
 MON=1 OSD=1 MDS=1 MGR=1 ../src/vstart.sh --debug --new -x --localhost --bluestore --bluestore-devs ${blkdev}
+
+mkdir -p /etc/ceph
 cp ceph.conf /etc/ceph/ceph.conf
 cp keyring /etc/ceph/keyring
 ./bin/ceph -s
