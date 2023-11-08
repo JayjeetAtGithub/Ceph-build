36b88da45226f1bda179c5a2e60d5556791327b7
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-11-08 13:44:31 -0800

    do ninja install

--- build-ceph.sh
+++ build-ceph.sh
@@ -21,3 +21,5 @@ rm -rf build
 ./do_cmake.sh -DWITH_RDMA=OFF -DWITH_RBD=OFF -DWITH_KRBD=OFF -DWITH_RADOSGW=OFF -DWITH_MANPAGE=OFF -DWITH_BABELTRACE=OFF -DWITH_MGR_DASHBOARD_FRONTEND=OFF
 cd build
 ninja -j$(nproc)
+mkdir -p /mnt/data/ceph-build/ceph/src/pybind/mgr/dashboard/frontend/dist
+ninja install
