5607d8610a2a6c1ae5b42ca31b2ea67466005f03
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-11-08 13:53:57 -0800

    use built binaries to delete pools

--- stop-ceph.sh
+++ stop-ceph.sh
@@ -4,14 +4,15 @@ set -eu
 blkdev=$1
 
 # delete the cephfs
+cd ceph/build
+
 fusermount -uz /mnt/cephfs || true
 rm -rf /mnt/cephfs
-ceph fs fail cephfs || true
-ceph fs rm cephfs --yes-i-really-mean-it || true
-ceph osd pool delete cephfs_data cephfs_data --yes-i-really-really-mean-it || true
-ceph osd pool delete cephfs_metadata cephfs_metadata --yes-i-really-really-mean-it || true
+bin/ceph fs fail cephfs || true
+bin/ceph fs rm cephfs --yes-i-really-mean-it || true
+bin/ceph osd pool delete cephfs_data cephfs_data --yes-i-really-really-mean-it || true
+bin/ceph osd pool delete cephfs_metadata cephfs_metadata --yes-i-really-really-mean-it || true
 
-cd ceph/build
 ../src/stop.sh
 
 wipefs -af ${blkdev}
